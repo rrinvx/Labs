d4ac89f729c5c2e0af1da4579d51ae81ff7e0edf
rrinvx <rina268183@gmail.com>
2024-11-14 15:15:53 +0200

    Add files via upload

---
+++ Lab.5/Lab.5.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+const int MAX_ACCOUNTS = 100;
+
+void createAccount(string& owner, double& balance);
+void topUp(double* balance);
+void withdraw(double* balance);
+void displayAccounts(const string owners[], const double balances[], int accountCount);
+
+int main()
+{
+    string owners[MAX_ACCOUNTS];
+    double balances[MAX_ACCOUNTS];
+    int accountCount = 0;
+    int choice;
+
+    do
+    {
+        cout << "\n Меню \n";
+        cout << "1. Створити рахунок\n";
+        cout << "2. Поповнити рахунок\n";
+        cout << "3. Зняти гроші з рахунку\n";
+        cout << "4. Інформація про рахунок\n";
+        cout << "5. Вихід\n";
+        cout << "Виберіть опцію: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            if (accountCount < MAX_ACCOUNTS)
+            {
+                createAccount(owners[accountCount], balances[accountCount]);
+                accountCount++;
+            }
+            else
+            {
+                cout << "Досягнуто макс. кількості облікових записів.\n";
+            }
+            break;
+        case 2:
+        {
+            int accountNumber;
+            cout << "Введіть номер ррахунку: ";
+            cin >> accountNumber;
+            if (accountNumber >= 0 && accountNumber < accountCount)
+            {
+                topUp(&balances[accountNumber]);
+            }
+            else
+            {
+                cout << "Такого номеру рахунку не існує.\n";
+            }
+            break;
+        }
+        case 3:
+        {
+            int accountNumber;
+            cout << "Введіть номер рахунку: ";
+            cin >> accountNumber;
+            if (accountNumber >= 0 && accountNumber < accountCount)
+            {
+                withdraw(&balances[accountNumber]);
+            }
+            else
+            {
+                cout << "Такого номеру рахунку не існує.\n";
+            }
+            break;
+        }
+        case 4:
+            displayAccounts(owners, balances, accountCount);
+            break;
+        case 5:
+            cout << "Вихід з програми \n";
+            break;
+        default:
+            cout << "Невірний вибір,спробуйте ще \n";
+        }
+    } while (choice != 5);
+
+    return 0;
+}
+
+void createAccount(string& owner, double& balance)
+{
+    cout << "Введіть ім'я власника рахунку: ";
+    cin >> owner;
+    cout << "Введіть початковий баланс: ";
+    cin >> balance;
+    cout << owner << "Створив(-ла) рахунок з балансом: " << balance << "грн " << endl;
+}
+
+void topUp(double* balance)
+{
+    double amount;
+    cout << "Введіть суму поповнення: ";
+    cin >> amount;
+    if (amount > 0)
+    {
+        *balance += amount;
+        cout << "Оновлений баланс: " << *balance << "грн" << endl;
+    }
+   
+
+void withdraw(double* balance)
+{
+    double amount;
+    cout << "Введіть суму для зняття: ";
+    cin >> amount;
+    if (amount > 0 && *balance >= amount)
+    {
+        *balance -= amount;
+        cout << "Зняття виконано.Новий баланс: " << *balance << "грн" << endl;
+    }
+    else
+    {
+        cout << "Недостатньо коштів \n";
+    }
+}
+
+void displayAccounts(const string owners[], const double balances[], int accountCount)
+{
+    cout << "\n Список облікових записів: \n";
+    for (int i = 0; i < accountCount; i++)
+    {
+        cout << "Рахунок:  " << i + 1 << owners[i] << "Баланс: " << balances[i] << "грн" << endl;
+    }
+}
---
+++ Lab.6/Lab.6.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+using namespace std;
+
+const int MAX_PRODUCTS =100;
+
+string productNames[MAX_PRODUCTS];
+int productQuantities[MAX_PRODUCTS];
+double productPrices[MAX_PRODUCTS];
+int productCount = 0;
+
+void addProduct();
+void deliteProduct();
+void updateQuantity();
+void showProducts();
+void calculateTotalCost();
+
+int main()
+{
+    int choice;
+    do
+    {
+        
+        cout << "\n Меню управління складом \n";
+        cout << "1. Додати продукт\n";
+        cout << "2. Видалити товар за назвою\n";
+        cout << "3. Оновити кількість товару\n";
+        cout << "4. Показати всі товари\n";
+        cout << "5. Розрахунок загальної вартості товарів на складі.\n";
+        cout << "6. Вихід\n";
+        cout << "Виберіть опцію: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            addProduct();
+            break;
+        case 2:
+            deliteProduct();
+            break;
+        case 3:
+            updateQuantity();
+            break;
+        case 4:
+            showProducts();
+            break;
+        case 5:
+            calculateTotalCost();
+            break;
+        case 6:
+            cout << "Вихід.";
+            break;
+        default:
+            cout << "Невірний вибір. ";
+        }
+    } while (choice != 6);
+
+    return 0;
+}
+
+void addProduct()
+{
+    if (productCount < MAX_PRODUCTS)
+    {
+        cout << "Введіть ім'я товару: ";
+        cin >> productNames[productCount];
+        cout << "Введіть кількість товару: ";
+        cin >> productQuantities[productCount];
+        cout << "Введіть ціну товару: ";
+        cin >> productPrices[productCount];
+        productCount++;
+        cout << "Ви успішно додали товар.\n";
+    }
+    else
+    {
+        cout << "Склад заповнений,неможливо додати новий товар.\n";
+    }
+}
+
+void deliteProduct()
+{
+    string name;
+    cout << "Введіть назву товару для видалення: ";
+    cin >> name;
+    for (int i = 0; i < productCount; i++)
+    {
+        if (productNames[i] == name)
+        {
+            for (int j = i; j < productCount - 1; j++)
+            {
+                productNames[j] = productNames[j + 1];
+                productQuantities[j] = productQuantities[j + 1];
+                productPrices[j] = productPrices[j + 1];
+            }
+            productCount--;
+            cout << "Ви успішно видалили товар.\n";
+            return;
+        }
+    }
+    cout << "Товар не знайдений\n";
+}
+
+void updateQuantity()
+{
+    string name;
+    int newQuantity;
+    cout << "Введіть назву продукту,кількість якого хочете змінити:: ";
+    cin >> name;
+    cout << "Введіть кількість: ";
+    cin >> newQuantity;
+    for (int i = 0; i < productCount; i++)
+    {
+        if (productNames[i] == name)
+        {
+            productQuantities[i] = newQuantity;
+            cout << "Кількість оновлена.\n";
+            return;
+        }
+    }
+    cout << "Товар не знайдений.\n";
+}
+
+void showProducts()
+{
+    if (productCount == 0)
+    {
+        cout << "Товарів на складі немає.\n";
+    }
+    else
+    {
+        cout << "\n Список товарів на складі \n";
+        for (int i = 0; i < productCount; i++)
+        {
+            cout << "Назва: " << productNames[i]
+                << "Кількість: " << productQuantities[i]
+                << "Ціна: " << productPrices[i] << "грн" << endl;
+        }
+    }
+}
+
+void calculateTotalCost()
+{
+    double total = 0;
+    for (int i = 0; i < productCount; i++)
+    {
+        total += productQuantities[i] * productPrices[i];
+    }
+    cout << "Загальна вартість товарів на складі: " << total << "грн" << endl;
+}
